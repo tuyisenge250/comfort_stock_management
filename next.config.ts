41f030db6b4a65a92d45790beb35ae7a575573b4
tuyisenge250 <benjaminwell250@gmail.com>
2025-08-01 15:40:10 +0200

    type disabled

--- next.config.ts
+++ next.config.ts
@@ -11,8 +11,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true
+  },  typescript: {
+    ignoreBuildErrors: true,
   },
-
 };
 
 export default nextConfig;
